12b07ee9f8de23c53dc7f2fefb9af04565463c9d
Rex <dandylion13@gmail.com>
2014-07-24 14:14:14 -0400

    Updated CNAME and document paths

--- scripts/documents.js
+++ scripts/documents.js
@@ -2,8 +2,8 @@ var POETRYBOOK = POETRYBOOK || {};
 
 POETRYBOOK.documents = (function () {
   return [
-   "http://kittykatattack.github.io/kittykatattack/documents/book.markdown", 
-   "http://kittykatattack.github.io/kittykatattack/documents/git.markdown", 
-   "http://kittykatattack.github.io/kittykatattack/documents/storyMaker.markdown"
+   "../docments/book.markdown", 
+   "../documents/git.markdown", 
+   "../documents/storyMaker.markdown"
   ];
 }());
